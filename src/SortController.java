fce5e4d62d4e9a67e13fb0932d56c2a667246dcc
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2022-06-25 17:01:49 +0300

    Initial commit

---
+++ src/SortController.java
@@ -0,0 +1,24 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SortController implements Initializable {
+
+    @FXML
+    private GridPane grid;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void onSort() {
+    }
+
+    public void onShuffle() {
+    }
+}
---
+++ src/SortMain.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SortMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root= FXMLLoader.load(getClass().getResource("Sort.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("Sort Visualizer");
+            primaryStage.sizeToScene();
+            primaryStage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
